08d29666e811bd838482470cfe636336503947e6
Cillian Mc Neill <cillian.mcneill@mastercard.com>
2018-08-29 11:07:24 +0100

    Seperating the functions of the UI

--- src/main/resources/static/driver.js
+++ src/main/resources/static/driver.js
@@ -7,22 +7,19 @@
 //});
 
 
-
 var TIMEOUT = 2000; // ms
 
 function b64dec(str) {
-  if (str.length % 4 == 1) {
-    throw "Improper b64 string";
-  }
+    if (str.length % 4 == 1) {
+        throw "Improper b64 string";
+    }
 
-  var b64 = str;
-  while (b64.length % 4 != 0) {
-    b64 += "=";
-  }
-  return new Uint8Array(base64js.toByteArray(b64));
+    var b64 = str;
+    while (b64.length % 4 != 0) {
+        b64 += "=";
+    }
+    return new Uint8Array(base64js.toByteArray(b64));
 }
-
-
 
 
 var preformatMakeCredReq = (makeCredReq) => {
@@ -33,17 +30,17 @@ var preformatMakeCredReq = (makeCredReq) => {
 }
 
 var publicKeyCredentialToJSON = (pubKeyCred) => {
-    if(pubKeyCred instanceof Array) {
+    if (pubKeyCred instanceof Array) {
         let arr = [];
-        for(let i of pubKeyCred)
+        for (let i of pubKeyCred)
             arr.push(publicKeyCredentialToJSON(i));
         return arr
     }
 
-    if(pubKeyCred instanceof ArrayBuffer) {
+    if (pubKeyCred instanceof ArrayBuffer) {
         return base64url.encode(pubKeyCred)
     }
-    if(pubKeyCred instanceof Object) {
+    if (pubKeyCred instanceof Object) {
         let obj = {};
 
         for (let key in pubKeyCred) {
@@ -58,7 +55,7 @@ var publicKeyCredentialToJSON = (pubKeyCred) => {
 
 var preformatGetAssertReq = (getAssert) => {
     getAssert.challenge = base64url.decode(getAssert.challenge);
-    for(let allowCred of getAssert.allowCredentials) {
+    for (let allowCred of getAssert.allowCredentials) {
         allowCred.id = base64url.decode(allowCred.id);
     }
     return getAssert
@@ -78,8 +75,9 @@ function init() {
     clearError();
     clearLoading();
     hideForms();
-    if(isBrowserCompatible()){
+    if (isBrowserCompatible()) {
         $("#registerForm").show();
+        $("#loginForm").show();
     } else {
         displayError("Incompatible browser.");
     }
@@ -105,7 +103,7 @@ function hideForms() {
     $("#loginForm").hide();
 }
 
-function displayLoading(message){
+function displayLoading(message) {
     hideForms();
     clearSuccess();
     $("#loadingText").text(message);
@@ -117,34 +115,33 @@ function clearError() {
     $("#error").hide();
 }
 
-function switchToLogin() {
+function goHome() {
     clearLoading();
     clearError();
-    $("#registerForm").hide();
-    $("#loginForm").show();
+    window.history.back();
 }
 
-function displaySuccess(message){
+function displaySuccess(message) {
     hideForms();
     clearLoading();
     $("#successMessage").text(message);
     $("#success").show();
 }
 
-function clearSuccess(){
+function clearSuccess() {
     $("#successMessage").text();
     $("#success").hide();
 }
 
 function processRegisterForm(e) {
-    if(e.preventDefault) e.preventDefault();
+    if (e.preventDefault) e.preventDefault();
     hideForms();
     clearSuccess();
     displayLoading("Contacting token... please perform your verification gesture (e.g., touch it, or plug it in)\n\n");
 
 //    let rpid = 'gh-50v0y52.corp.mastercard.org';
     let rpid = document.domain;
-    let formBody = {"username": $("#username").val(),"displayName":$("#alias").val()};
+    let formBody = {"username": $("#username").val(), "displayName": $("#alias").val()};
     fetch('/attestation/options', {
         method: 'POST',
         credentials: 'include',
@@ -153,35 +150,47 @@ function processRegisterForm(e) {
         },
         body: JSON.stringify(formBody)
     })
-    .then((response) => response.json())
-    .then((response) => {
-            if(response.status !== 'ok')
-                throw new Error(`Server responed with error. The message is: ${response.message}`);
-            let v = preformatMakeCredReq(response);
-            console.info("Updated Response from FIDO RP server ", v)
-            console.info("RP Domain = ", rpid)
-            v.rp.id = rpid;
-            navigator.credentials.create({ publicKey: v})
-                .then(function (aNewCredentialInfo){
-                       var response = publicKeyCredentialToJSON(aNewCredentialInfo);
-                       console.info("response = " + response)
-                       console.info("response = " + JSON.stringify(response))
+        .then((response) => response.json())
+        .then((response) => {
+                if (response.status !== 'ok')
+                    throw new Error(`Server responed with error. The message is: ${response.message}`);
+                let v = preformatMakeCredReq(response);
+                console.info("Updated Response from FIDO RP server ", v)
+                console.info("RP Domain = ", rpid)
+                v.rp.id = rpid;
+                navigator.credentials.create({publicKey: v})
+                    .then(function (aNewCredentialInfo) {
+                        var response = publicKeyCredentialToJSON(aNewCredentialInfo);
+                        console.info("response = " + response)
+                        console.info("response = " + JSON.stringify(response))
                         fetch('/attestation/result', {
-                            method: 'POST',
-                            credentials: 'include',
-                            headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(response)
-                        }
+                                method: 'POST',
+                                credentials: 'include',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify(response)
+                            }
+                        ).then(
+                            response => {
+                                response.json().then(
+                                    data => {
+                                        if (response.status === 200) {
+                                            displaySuccess("Successful registration!")
+                                            goHome();
+                                        } else {
+                                            displayError(data)
+                                        }
+                                    }
+                                )
+                            }
                         )
-                        switchToLogin();
-                }).catch(function(error){
-                               console.error("respones = " + error)
-                           }
-                       )
+                    }).catch(function (error) {
+                        console.error("respones = " + error)
+                    }
+                )
 
-        }
+            }
         )
 
     return false;
@@ -199,7 +208,7 @@ function processLoginForm(e) {
     $("#getOut").text("Contacting token... please perform your verification gesture (e.g., touch it, or plug it in)\n\n");
 
     let rpid = document.domain;
-    let formBody = {"username": $("#loginUsername").val(), "documentDomain":rpid};
+    let formBody = {"username": $("#loginUsername").val(), "documentDomain": rpid};
 
     fetch('/assertion/options', {
         method: 'POST',
@@ -209,36 +218,41 @@ function processLoginForm(e) {
         },
         body: JSON.stringify(formBody)
     })
-    .then((response) => response.json())
-    .then((response) => {
-            if(response.status !== 'ok')
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.status !== 'ok')
                 throw new Error(`Server responed with error. The message is: ${response.message}`);
-                console.info("Updated Response from FIDO RP server ", response)
-                var resp = preformatGetAssertReq(response)
-                console.info("Updated Response from FIDO RP server ", resp)
-                navigator.credentials.get({publicKey: resp})
-                .then(function(aAssertion) {
+            console.info("Updated Response from FIDO RP server ", response)
+            var resp = preformatGetAssertReq(response)
+            console.info("Updated Response from FIDO RP server ", resp)
+            navigator.credentials.get({publicKey: resp})
+                .then(function (aAssertion) {
                     var resp = JSON.stringify(publicKeyCredentialToJSON(aAssertion));
                     console.info("Get Assertion Response " + response);
-                        fetch('/assertion/result', {
-                            method: 'POST',
-                            credentials: 'include',
-                            headers: {
+                    fetch('/assertion/result', {
+                        method: 'POST',
+                        credentials: 'include',
+                        headers: {
                             'Content-Type': 'application/json'
-                            },
+                        },
                         body: resp
-                    })
-                    .then(function(data) {
-                        if(data.ok && data.status === 200){
-                            displaySuccess("Successful match ["+data.status+"-"+data.type+"]");
-                        } else {
-                            displayError("Failure ["+data.status+"-"+data.type+"]");
+                    }).then(
+                        response => {
+                            response.json().then(
+                                data => {
+                                    if (response.status === 200) {
+                                        displaySuccess("Successful match [" + data.status + "-" + data.type + "]");
+                                    } else {
+                                        displayError("Failure [" + data.status + "-" + data.type + "]");
+                                    }
+                                }
+                            )
                         }
-                    })
-            }).catch(function (aErr) {
+                    )
+                }).catch(function (aErr) {
                 console.info("Unable to get Assertion Response ", JSON.stringify(aErr))
             });
-    });
+        });
 }
 
 
@@ -246,10 +260,10 @@ function isBrowserCompatible() {
     return navigator && navigator.credentials && typeof (navigator.credentials.create) === 'function';
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     init();
 
-  $("#getButton").click(function() {
+    $("#getButton").click(function () {
 
     });
 });
